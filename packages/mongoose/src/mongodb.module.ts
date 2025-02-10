b61deba317a65d0f24cb0e1bbfe22b9cd6d73f50
Kristiyan Tachev <kristiyan.tachev@tallarna.com>
2025-02-10 09:15:56 +0200

    fix(mongoose): added validation for mongoose url if it is missing

--- packages/mongoose/src/mongodb.module.ts
+++ packages/mongoose/src/mongodb.module.ts
@@ -10,12 +10,23 @@ export class MongoDbModule {
     url: string,
     options?: ConnectOptions
   ): ModuleWithProviders {
+    if (!url) {
+      throw new Error('Missing MongoDB Url inside MongoDbModule.forRoot(...)');
+    }
     return {
       module: MongoDbModule,
       providers: [
         {
           provide: MONGOOSE_CONNECTION_TOKEN,
-          useFactory: () => mongooseInstance.connect(url, options),
+          useFactory: async () => {
+            try {
+              return await mongooseInstance.connect(url, options);
+            } catch (e) {
+              console.error('Unable to initialize mongodb connection');
+              console.error(e);
+              throw new Error(e);
+            }
+          },
         },
       ],
     };
